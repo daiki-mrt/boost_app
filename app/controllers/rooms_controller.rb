a576a7091275c7227dff9c2c100d5d47ed20b974
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-26 19:13:20 +0900

    ルームの新規作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,24 @@
 class RoomsController < ApplicationController
   def index
   end
+
+  def new
+    @room = Room.new()
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.valid?
+      @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+
+  private
+  def room_params
+    params.require(:room).permit(:name, :text).merge(user_ids: current_user.id)
+  end
 end
