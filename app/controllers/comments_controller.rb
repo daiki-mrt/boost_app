edaa09401110cfaa8fc1456fa2fb478fe3206699
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-27 14:27:05 +0900

    コメント投稿機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to room_path(params[:room_id])
+    else
+      @room = Room.find(params[:room_id])
+      @comments = Comment.includes(:user)
+      render "rooms/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, room_id: params[:room_id])
+  end
+end
