8b83d4bd4559e7ba91ff1ac974fa7ae90a97a445
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-10-27 18:56:23 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/show'
   root to: "rooms#index"
   devise_for :users
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#new_guest"
   end
+  resources :users, only: :show
 
   resources :rooms do
     resources :comments, only: :create
